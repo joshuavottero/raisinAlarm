bd91ddc65a4dfbb55da0ae8f821cc8dd253ffcc0
joshuavottero <joshuavottero@outlook.com>
2021-11-24 23:54:59 +0000

    made a raisin alram

---
+++ raisin.js
@@ -0,0 +1,15 @@
+
+const raisinAlarm = function(cookie) {
+  for (let i = 0; i < cookie.length; i++) {
+    if ("🍇" === cookie[i]) {
+      return "Raisin alert!";
+    }
+  }
+  return "All good!";
+};
+
+console.log(raisinAlarm(["🍫", "🍫", "🍇", "🍫"]));
+console.log(raisinAlarm(["🍫", "🍇", "🍫", "🍫", "🍇" ]));
+console.log(raisinAlarm(["🍫", "🍫", "🍫"]));
+
+
